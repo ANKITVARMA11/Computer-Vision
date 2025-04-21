4b81aaa408e9c67fadd5f9f51dd85c984ad2487f
ANKITVARMA11 <ankitvarma3708@gmail.com>
2025-04-21 22:27:59 +0530

    Image Basics with OpenCV

---
+++ OpenCV1.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread("Computer-Vision-with-Python/DATA/00-puppy.jpg")
+
+while True:
+    
+    cv2.imshow("Puppy",img)
+
+    # If we've waited at least 1ms AND we've pressed the ESC
+    if cv2.waitKey(1) & 0XFF == 27:
+        break
+
+cv2.destroyAllWindows()
